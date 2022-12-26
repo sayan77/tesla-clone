c6079fd1681ed89bf16023da38b5c302677b513d
Sayan <sayaniter@gmail.com>
2022-12-27 00:19:16 +0530

    Initial commit of Header

---
+++ src/components/Header.js
@@ -0,0 +1,66 @@
+import React from 'react'
+import styled from 'styled-components'
+import MenuIcon from '@mui/icons-material/Menu';
+
+function Header() {
+  return (
+    <Container>
+        <a href='#'><img src='/images/logo.svg' /></a>
+
+        <MenuGroup>
+            <div> Model S </div>
+            <div> Model X </div>
+            <div> Model Y </div>
+        </MenuGroup>
+
+        <RightMenu>
+            <a href='#'> Shop </a>
+            <a href='#'> Tesla Account </a>
+
+            <MenuIconContainer>
+              <MenuIcon />
+            </MenuIconContainer>
+        </RightMenu>
+
+    </Container>
+  )
+}
+
+export default Header
+
+
+const Container = styled.div`
+ display : flex;
+ min-height : 60px; 
+ align-items : center;
+ justify-content : space-between;
+ padding : 0 20px; 
+`
+
+const MenuGroup = styled.div`
+ display : flex;
+ text-transform : uppercase;
+ div {
+  font-weight : 600;
+  padding : 0 10px; 
+  cursor : pointer; 
+ }
+
+`
+
+const RightMenu = styled.div`
+display : flex;
+align-items : center; 
+a {
+  font-weight : 600;
+  text-transform : uppercase;
+  padding-right : 10px;
+}
+`
+
+const MenuIconContainer = styled.div`
+ display : flex; 
+ align-items : center; 
+ cursor : pointer;
+
+`
